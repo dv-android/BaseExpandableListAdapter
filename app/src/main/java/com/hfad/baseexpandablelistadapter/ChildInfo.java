5112e2184a22bee4996295fd32c51af48156af61
dv-android <devang.droid@gmail.com>
2017-08-04 18:32:16 +0530

    Added to Git Repo

---
+++ app/src/main/java/com/hfad/baseexpandablelistadapter/ChildInfo.java
@@ -0,0 +1,15 @@
+package com.hfad.baseexpandablelistadapter;
+
+public class ChildInfo {
+
+    private String playerName = "";
+
+    public String getName() {
+        return playerName;
+    }
+
+    public void setName(String playerName) {
+        this.playerName = playerName;
+    }
+
+}
---
+++ app/src/main/java/com/hfad/baseexpandablelistadapter/GroupInfo.java
@@ -0,0 +1,26 @@
+package com.hfad.baseexpandablelistadapter;
+
+import java.util.ArrayList;
+
+public class GroupInfo {
+
+    private String teamName;
+    private ArrayList<ChildInfo> list = new ArrayList<ChildInfo>();
+
+    public String getName() {
+        return teamName;
+    }
+
+    public void setName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public ArrayList<ChildInfo> getPlayerName() {
+        return list;
+    }
+
+    public void setPlayerName(ArrayList<ChildInfo> playerName) {
+        this.list = playerName;
+    }
+
+}
